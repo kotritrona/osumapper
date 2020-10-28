1db17dfbc20918caab8169c147a8735ee582f12e
kotritrona <kotritrona@gmail.com>
2020-10-28 19:12:05 +1100

    Generic Commit Message

--- v7.0/act_timing.py
+++ v7.0/act_timing.py
@@ -12,7 +12,7 @@ from shutil import copy
 from timing import *
 from metadata import *
 
-def get_timed_osu_file(music_path, input_filename = "assets/template.osu", output_filename = "timing.osu"):
+def get_timed_osu_file(music_path, input_filename = "assets/template.osu", output_filename = "timing.osu", game_mode = 0, mania_key_count = None):
     with open(input_filename) as osu_file:
         osu_text = osu_file.read()
 
@@ -27,11 +27,15 @@ def get_timed_osu_file(music_path, input_filename = "assets/template.osu", outpu
     bpm, offset = get_timing(music_path)
 
     osu_text = re.sub("{audio_filename}",     "audio.mp3", osu_text)
+    osu_text = re.sub("{game_mode}",          game_mode, osu_text)
     osu_text = re.sub("{artist}",             artist, osu_text)
     osu_text = re.sub("{title}",              title, osu_text)
     osu_text = re.sub("{version}",            get_difficulty_name(), osu_text)
     osu_text = re.sub("{hp_drain}",           "{}".format(np.random.randint(0, 101) / 10), osu_text)
-    osu_text = re.sub("{circle_size}",        "{}".format(np.random.randint(30, 51) / 10), osu_text)
+    if mania_key_count is None:
+        osu_text = re.sub("{circle_size}",    "{}".format(np.random.randint(30, 51) / 10), osu_text)
+    else:
+        osu_text = re.sub("{circle_size}",    "{}".format(mania_key_count), osu_text)
     osu_text = re.sub("{overall_difficulty}", "{}".format(np.random.randint(50, 91) / 10), osu_text)
     osu_text = re.sub("{approach_rate}",      "{}".format(np.random.randint(70, 96) / 10), osu_text)
     osu_text = re.sub("{slider_velocity}",    "{}".format(np.random.randint(12, 26) / 10), osu_text)
---
+++ v7.0/mania_setup_colab.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+
+#
+# Colab functions
+#
+
+import os
+
+def colab_clean_up(input_file_name):
+    for item in [input_file_name, "mapthis.json", "audio.mp3", "timing.osu", "rhythm_data.npz", "mapthis.npz"]:
+        try:
+            os.remove(item);
+        except:
+            pass
+    print("intermediate files cleaned up!")
+
+def load_pretrained_model(model_name):
+    model_data = {
+        "default" : {
+            "rhythm_model" : "models/{}/rhythm_model".format(model_name),
+            "pattern_dataset" : "models/{}/mania_pattern_dataset.npz".format(model_name),
+            "rhythm_param" : [0.5, 0.2, [0, 0, 0, 0], 8, 1, 4],
+            "modding" : {
+                "remove_trills" : 3
+            }
+        },
+        "lowkey" : {
+            "rhythm_model" : "models/mania_lowkey/rhythm_model".format(model_name),
+            "pattern_dataset" : "models/mania_pattern/mania_pattern_dataset.npz".format(model_name),
+            "rhythm_param" : [0.65, 0.4, [0, 0, 0, 0], 8, 5, 4],
+            "modding" : {
+                "remove_trills" : 3
+            }
+        },
+        "highkey" : {
+            "rhythm_model" : "models/mania_highkey/rhythm_model".format(model_name),
+            "pattern_dataset" : "models/mania_pattern/mania_pattern_dataset.npz".format(model_name),
+            "rhythm_param" : [0.45, 0.12, [0, 0, 0, 0], 8, 5, 4],
+            "modding" : {
+                "remove_trills" : 3
+            }
+        }
+    };
+    if model_name not in model_data:
+        return model_data["default"];
+    return model_data[model_name];
