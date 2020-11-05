fc42fdefa54df775db4763794cf23a91443a9922
kotritrona <kotritrona@gmail.com>
2020-11-06 03:24:20 +1100

    Fix path problem

--- v7.0/act_newmap_prep.py
+++ v7.0/act_newmap_prep.py
@@ -5,12 +5,16 @@
 #
 
 from audio_tools import *;
+from os_tools import *
 
 import os, re, time;
 
 mapdata_path = "mapdata/";
 
 def step4_read_new_map(file_path, divisor = 4):
+    # fix the path..?
+    fix_path()
+
     # Test paths and node
     test_process_path("node");
 
--- v7.0/os_tools.py
+++ v7.0/os_tools.py
@@ -9,3 +9,8 @@ import re, os, subprocess, json;
 def run_command(str_array):
     x = subprocess.Popen(str_array, stdout=subprocess.PIPE);
     return x.stdout.read();
+
+def fix_path():
+    path = os.path.dirname(__file__)
+    if len(path) > 1:
+        os.chdir(path)
