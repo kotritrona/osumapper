1355d7aeb27cd105faaddbd82430e81ab74a8bcc
kotritrona <kotritrona@gmail.com>
2020-11-06 04:24:15 +1100

    Add node_modules error message

--- v7.0/os_tools.py
+++ v7.0/os_tools.py
@@ -7,7 +7,10 @@
 import re, os, subprocess, json;
 
 def run_command(str_array):
-    x = subprocess.Popen(str_array, stdout=subprocess.PIPE);
+    x = subprocess.Popen(str_array, stdout=subprocess.PIPE, stderr=subprocess.PIPE);
+    err = x.stderr.read()
+    if len(err) > 1:
+        print(err.decode("utf8"))
     return x.stdout.read();
 
 def fix_path():
